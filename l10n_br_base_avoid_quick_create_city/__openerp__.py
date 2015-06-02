fdeaed4b396ef1fa26b0f5bd3fcba149c034bab3
Luis Felipe Mileo <mileo@kmee.com.br>
2015-06-02 19:46:01 -0300

    [REF][MIG] Repo migration to 8.0, module avoid create city:  split and module migration

---
+++ l10n_br_base_avoid_quick_create_city/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    KMEE Avoid Quick Create City, State and Country module for OpenERP
+#    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
+#    @author Matheus Lima Felix <matheus.felix@kmee.com.br>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'l10n_br_base Avoid Quick Create City, State and Country',
+    'version': '0.1',
+    'category': 'Generic Modules',
+    'description': """l10n_br_base Avoid Quick Create City, State and Country on
+    ResPartner Form""",
+    'author': 'KMEE',
+    'license': 'AGPL-3',
+    'website': 'http://www.kmee.com.br',
+    'depends': [
+        'web_m2x_options',
+        'l10n_br_base',
+    ],
+    'data': [
+        'view/res_partner_view.xml',
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': True,
+}
