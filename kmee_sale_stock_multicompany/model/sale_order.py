806ab4934568643374611a73225742a0f7c4b334
Hendrix Costa <hendrixcosta@gmail.com>
2016-05-18 20:10:52 -0300

    [FIX] migraçao para API v8

--- kmee_sale_stock_multicompany/model/sale_order.py
+++ kmee_sale_stock_multicompany/model/sale_order.py
@@ -20,70 +20,96 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp.osv import fields, orm
-from openerp import SUPERUSER_ID
+from openerp import SUPERUSER_ID, models, api, fields
 
 
-class ProductStockMultiCompany(orm.TransientModel):
-    """ Sale Order Stock"""
-    _name = "product.stock_multicompany" 
+class ProductStockMultiCompany(models.TransientModel):
 
-    _columns = {
-                'product_id': fields.many2one('product.product', 'Product'),
-                'company_id':fields.many2one('res.company','Company'),
-                'stock_location':fields.many2one('stock.location','Localização'),
-                'qty_available': fields.float(string='Quantity On Hand'), #_product_qty_available, type='float', 
-                'virtual_available': fields.float(string='Quantity Available'), #_product_virtual_available, type='float', 
-                'line_id': fields.many2one('sale.order.line', 'Order Line Reference', readonly=True, states={'draft':[('readonly',False)]}),
-                }
+    _name = 'product.stock_multicompany'
 
-    def get_stock(self, cr, uid, product_id, context):
-        
+    product_id = fields.Many2one(
+        string = 'Product',
+        comodel_name = 'product.product',
+    )
+
+    company_id = fields.Many2one(
+        comodel_name = 'res.company',
+        string = 'Company'
+    )
+
+    stock_location = fields.Many2one(
+        comodel_name = 'stock.location',
+        string = 'Localização'
+    )
+
+    qty_available = fields.Float(
+        string='Quantity On Hand', #_product_qty_available, type='float',
+    )
+
+    virtual_available = fields.Float(
+        string='Quantity Available', #_product_virtual_available, type='float',
+    )
+
+    line_id = fields.Many2one(
+        comodel_name = 'sale.order.line',
+        string = 'Order Line Reference',
+        readonly=True,
+        states={'draft':[('readonly',False)]},
+    )
+
+    @api.multi
+    def get_stock(self, product_id):
+
         result = []
-        location_obj = self.pool.get('stock.location')
-        
-        
-        location_ids = location_obj.search(cr, SUPERUSER_ID, [('usage','in',['internal','supplier'])])
-        for location in location_obj.browse(cr, SUPERUSER_ID, location_ids, context):
-            
-            qty = self.pool.get('product.product')._product_available(cr, uid, [product_id], context=dict(context or {}, location=location.id))
-            # stock = location_obj._product_get_multi_location(cr, SUPERUSER_ID, [location.id], [product_id])
-            #
-            result.append(self.create(cr, uid, {'product_id': product_id,
-                                                'company_id': location.company_id.id,
-                                                'stock_location': location.id,
-                                                'qty_available': qty[product_id]['qty_available'],
-                                                'virtual_available': qty[product_id]['virtual_available'],
-                                                }, ))
+        location_obj = self.env['stock.location']
+        locations = location_obj.search([('usage','in',['internal','supplier'])])
+
+        for location in locations:
+            # cria um objeto de product
+            product_obj = self.env['product.product']
+            # adiciona o location no context
+            product_obj = product_obj.with_context(location= location.id)
+            # adiciona id do produto procurado no context
+            product_obj._ids = [product_id]
+            # chama metodo da classe que retorna a disponibilidade no location
+            qty = product_obj._product_available()
+            # cria instancia para cada location das informacoes de disponibilidade e apenda em um vetor
+            product_stock_multicompany_obj = (self.create({'product_id': product_id, 'company_id': location.company_id.id,
+                                                    'stock_location': location.id, 'qty_available': qty[product_id]['qty_available'],
+                                                    'virtual_available': qty[product_id]['virtual_available'],}))
+            result.append(product_stock_multicompany_obj.id)
+
+        # retorna vetor com disponibilidade de determinado produto em todos os locations
         return result
 
-class SaleOrderLine(orm.Model):
-    """ Sale Order """
+class SaleOrderLine(models.Model):
 
     _inherit = "sale.order.line"
-    _columns = {
-                'stock_line_id': fields.one2many('product.stock_multicompany', 'line_id', 'Order Lines', readonly=True),
-                }
-    
-    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+
+    stock_line_id = fields.One2many(
+        comodel_name = 'product.stock_multicompany',
+        inverse_name = 'line_id',
+        string = 'Order Lines',
+        readonly=True,
+    )
+
+    @api.multi
+    def product_id_change(self, pricelist, product, qty=0,
                           uom=False, qty_uos=0, uos=False, name='',
                           partner_id=False, lang=False, update_tax=True,
                           date_order=False, packaging=False,
-                          fiscal_position=False, flag=False, context=None):
-      
+                          fiscal_position=False, flag=False):
+
         result_super = super(SaleOrderLine, self).product_id_change(
-            cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name,
+            pricelist, product, qty, uom, qty_uos, uos, name,
             partner_id, lang, update_tax, date_order, packaging,
-            fiscal_position, flag, context)
-        
-        product_stock = self.pool.get('product.stock_multicompany')
-        
-        result = {}
-        
+            fiscal_position, flag)
+
         if not product:
             return result_super
-        
-        result['stock_line_id'] = product_stock.get_stock(cr, uid, product, context)        
+
+        result = {}
+        product_stock = self.env['product.stock_multicompany']
+        result['stock_line_id'] = product_stock.get_stock(product)
         result_super['value'].update(result)
         return result_super
