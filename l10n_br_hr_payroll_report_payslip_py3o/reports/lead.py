2be2a305bb929a76ee645e02ef4e867f4888ee54
vonpupp <vonpupp@gmail.com>
2017-02-08 19:30:37 -0200

    Add basic odt template

--- l10n_br_hr_payroll_report_payslip_py3o/reports/lead.py
+++ l10n_br_hr_payroll_report_payslip_py3o/reports/lead.py
@@ -2,6 +2,7 @@
 from openerp.addons.report_py3o.py3o_parser import py3o_report_extender
 
 
-@py3o_report_extender("crm.lead")
-def lead_report(pool, cr, uid, local_context, context):
+@py3o_report_extender("hr.payslip")
+def payslip_report(pool, cr, uid, local_context, context):
+    import ipdb; ipdb.set_trace() # BREAKPOINT
     local_context.update({})
