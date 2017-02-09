fa7bb24228c71bbec256bf30d2f289468ceaeab4
vonpupp <vonpupp@gmail.com>
2017-02-09 18:05:27 -0200

    Format template

--- l10n_br_hr_payroll_report_payslip_py3o/reports/lead.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-from openerp.addons.report_py3o.py3o_parser import py3o_report_extender
-
-
-@py3o_report_extender("hr.payslip")
-def payslip_report(pool, cr, uid, local_context, context):
-    import ipdb; ipdb.set_trace() # BREAKPOINT
-    local_context.update({})
