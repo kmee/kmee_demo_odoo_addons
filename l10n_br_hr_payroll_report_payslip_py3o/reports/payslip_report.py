911cf8239bc55dcda169b413a767beab1f24189e
vonpupp <vonpupp@gmail.com>
2017-02-10 14:55:15 -0200

    [ADD] Base fields for calculations

--- l10n_br_hr_payroll_report_payslip_py3o/reports/payslip_report.py
+++ l10n_br_hr_payroll_report_payslip_py3o/reports/payslip_report.py
@@ -10,32 +10,26 @@
 from openerp.addons.report_py3o.py3o_parser import py3o_report_extender
 
 
-class DummyClass():
-    def __init__(self):
-        self.campo = 'asdf'
-
-    def metodo(self):
-        return 'asdf'
-
-
-#"py3o_template_example"
+# l10n_br_hr_payslip_py3o_report.report
 @py3o_report_extender("l10n_br_hr_payroll_report_payslip_py3o.report_payslip_py3o_report")
 def payslip_report(pool, cr, uid, local_context, context):
-    print(context)
-    print(local_context)
-    print(pool)
-    print(cr)
-    print(uid)
-    dc = DummyClass()
-    #data_formatada = formata_data(payslip.date_from, u'%A/%Y')
-    #pybrasil.data.formata_data
+    #import pdb; pdb.set_trace() # BREAKPOINT
+    #employee_id = self.env['employee_id']
+    #employee = pool.get('hr.employee')
+    #slip_line = pool.get('hr.payslip.line')
+    #m = model('hr.payslip.line')
+    #id = m.search([('code', '=', 'BASE_INSS'), ('slip_id', '=', xxx)])
+    #m.browse(id).read()
+    # data_formatada = formata_data(payslip.date_from, u'%A/%Y')
+    # pybrasil.data.formata_data
 #    import pdb; pdb.set_trace() # BREAKPOINT
-    #local_context.update({})
-    #local_context.update(dc)
-    values = {
-        'base_inss': 0.0,
-        'base_fgts': 0.0,
-        'fgts': 0.0,
-        'base_irrf': 0.0,
-    }
-    local_context.update(values)
+    #values = {
+    #    #'logo': context['logo'],
+    #    'base_inss': 0.0,
+    #    'base_fgts': 0.0,
+    #    'fgts': 0.0,
+    #    'base_irrf': 0.0,
+    #}
+    #local_context.update(values)
+    #import pdb; pdb.set_trace() # BREAKPOINT
+    local_context.update({'logo': local_context['logo']})
