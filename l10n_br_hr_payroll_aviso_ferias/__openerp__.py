abae2f3e2694edb1ea62dcefb8767f29b1ee09d5
Hendrix Costa <hendrixcosta@gmail.com>
2017-02-20 19:00:36 -0300

    [ADD] Modulo para impressao do aviso de férias

---
+++ l10n_br_hr_payroll_aviso_ferias/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Aviso de Ferias for l10n_br_hr_payslip',
+    'summary': 'Aviso de Ferias for l10n_br_hr_payslip',
+    'version': '8.0.1.0.0',
+    'category': 'Reports',
+    'website': 'http://www.kmee.com.br',
+    'author': "KMEE, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    'depends': [
+        'l10n_br_hr_payroll',
+    ],
+    'data': [
+        'reports/reports.xml',
+    ],
+    'installable': True,
+}
