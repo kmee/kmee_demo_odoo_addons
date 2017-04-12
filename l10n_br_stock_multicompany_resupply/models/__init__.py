79b0adefdc4593cbbf9d161e6ac8bbb5e488cd09
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-04-12 19:18:45 -0300

    squash! [FIX] retorno das funcoes action_assign() e action_done() do stock_move
    squash! [FIX] Foi preciso utilizar a funcao sudo() para que seja possivel que o estoquista realize uma transferencia entre dois armazens de empresas diferentes.
    
    Nova Mensagem de Commit:
    
    Permite Transferência faturada entre empresas
    
    Faz `sudo()` no `stock.move:action_move()` para que
    `stock.move`s consequentes (`move_dest_id`) de outras
    empresas sejam processados corretamente.
    
    Força a `company_id` durante cálculo de disponibilidade
    dos `stock.quant` para ser a `company_id` da location
    ao invés da do usuário, evitando problemas de enxergar a
    disponibilidade existente. Veja PR:
    
    https://github.com/odoo/odoo/pull/16189

--- l10n_br_stock_multicompany_resupply/models/__init__.py
+++ l10n_br_stock_multicompany_resupply/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import res_company
 from . import stock_move
+from . import stock_quant
 from . import stock_warehouse
---
+++ l10n_br_stock_multicompany_resupply/models/stock_quant.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 KMEE - www.kmee.com.br
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class StockQuant(models.Model):
+
+    _inherit = "stock.quant"
+
+    @api.model
+    def _quants_get_order(self, location, product, quantity, domain=(),
+                          orderby='in_date'):
+        """
+        Fix quant visibility to match the location instead of the user company
+        """
+        # This is necessary while the PR below is not merged:
+        # https://github.com/odoo/odoo/pull/16189
+        if location and 'force_company' not in self.env.context:
+            self = self.with_context(force_company=location.company_id.id)
+        return super(StockQuant, self)._quants_get_order(
+            location, product, quantity, domain=domain, orderby=orderby)
