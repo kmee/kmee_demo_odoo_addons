46875d4bb96515089d4bd2d41f914574f999a55a
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-06-12 16:15:52 -0300

    [ADD] Tabela com o total das rubricas de provento e desconto dos holerites

--- l10n_br_hr_payroll_report/reports/payslip_report_analitico.py
+++ l10n_br_hr_payroll_report/reports/payslip_report_analitico.py
@@ -31,6 +31,50 @@ def analytic_report(pool, cr, uid, local_context, context):
         payslips = \
             pool['hr.payslip.run']\
             .browse(cr, uid, context['active_id']).slip_ids
+    SQL_BUSCA = '''
+    SELECT 
+        salary_rule.code,
+        salary_rule.name,
+        sum(payslip_line.quantity) as quantity,
+        sum(payslip_line.total),
+        rule_category.code as category
+    FROM 
+        hr_payslip payslip
+        join hr_payslip_line payslip_line on payslip_line.slip_id = payslip.id
+        join hr_salary_rule salary_rule on salary_rule.id = payslip_line.salary_rule_id
+        join hr_salary_rule_category rule_category on rule_category.id = salary_rule.category_id
+    WHERE
+        payslip.mes_do_ano = {}
+    GROUP BY
+        salary_rule.code,
+        salary_rule.name,
+        category
+    ORDER BY
+        salary_rule.name;
+    '''
+    SQL_BUSCA = SQL_BUSCA.format(wizard.mes_do_ano)
+    cr.execute(SQL_BUSCA)
+    payslip_lines = cr.dictfetchall()
+    proventos = []
+    descontos = []
+    total_proventos = 0.0
+    total_descontos = 0.0
+
+    class rubrica_obj(object):
+        def __init__(self, rubrica):
+            self.code = rubrica['code']
+            self.name = rubrica['name']
+            self.quantity = rubrica['quantity']
+            self.sum = rubrica['sum']
+
+    for rubrica in payslip_lines:
+        if rubrica['category'] == 'PROVENTO':
+            proventos.append(rubrica_obj(rubrica))
+            total_proventos += rubrica['sum']
+        elif rubrica['category'] in ['DEDUCAO', 'INSS', 'IRPF']:
+            descontos.append(rubrica_obj(rubrica))
+            total_descontos += rubrica['sum']
+
 
     legal_name = payslips[0].company_id.legal_name
     cnpj_cpf = payslips[0].company_id.cnpj_cpf
@@ -43,8 +87,10 @@ def analytic_report(pool, cr, uid, local_context, context):
         'mes_do_ano': mes_do_ano,
         'ano': ano,
         'objects': payslips,
-        'totals': {
-            ''
-        },
+        'proventos': proventos,
+        'descontos': descontos,
+        'total_proventos': total_proventos,
+        'total_descontos': total_descontos,
+        'total_liquido': total_proventos - total_descontos
     }
     local_context.update(data)
