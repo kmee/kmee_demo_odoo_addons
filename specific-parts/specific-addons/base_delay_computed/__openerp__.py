c7d9aed29e83509d01e536ea140b1e0369aaadd8
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-06-20 10:53:54 -0300

    fixup! Infra p/ Postergar cálculo de campos computados
    Dependência incorreta, e adiciona descrição.

--- specific-parts/specific-addons/base_delay_computed/__openerp__.py
+++ specific-parts/specific-addons/base_delay_computed/__openerp__.py
@@ -5,11 +5,13 @@
 {
     'name': "Base Delay Computed",
     'summary': "Delays computed fields to the end of a context manager block",
+    'description': """
+    To use this module, it must be loaded on startup with, for example:
+
+        --load=web,web_kanban,base_delay_computed
+    """,
     'category': 'Tools',
     'version': '8.0.1.0.0',
-    'depends': [
-        'base_model_override',
-    ],
     'author': 'Leonardo Rochael Almeida <leorochael@gmail.com>, '
               'Odoo Community Association (OCA)',
     'website': 'https://odoo-community.org/',
