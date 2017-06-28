df11177562f682365514997dc707ca244699dc13
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-06-28 19:08:32 -0300

    [ADD] Buscar ultimo salario

--- l10n_br_hr_payroll_report/reports/payslip_report_rescisao.py
+++ l10n_br_hr_payroll_report/reports/payslip_report_rescisao.py
@@ -6,11 +6,34 @@ from openerp.addons.report_py3o.py3o_parser import py3o_report_extender
 from openerp import api
 
 
+def buscar_ultimo_salario(
+        pool, cr, uid, mes_da_rescisao, contract_id, context):
+    ultimo_holerite_id = pool['hr.payslip'].search(
+        cr, uid, [
+            ('contract_id', '=', contract_id),
+            ('mes_do_ano', '=', mes_da_rescisao - 1),
+            ('tipo_de_folha', '=', 'normal'),
+        ], context=context
+    )
+    ultimo_holerite = pool['hr.payslip'].browse(cr, uid, ultimo_holerite_id)
+    for line in ultimo_holerite.line_ids:
+        if line.code == "SALARIO":
+            return line.total
+
+
 @api.model
-@py3o_report_extender('l10n_br_hr_payroll.reports.payslip_report_rescisao')
+@py3o_report_extender(
+    'l10n_br_hr_payroll_report.report_payslip_py3o_rescisao')
 def payslip_rescisao(pool, cr, uid, local_context, context):
     companylogo = \
         pool['hr.payslip'] \
         .browse(cr, uid, context['active_id']).company_id.logo
-    d = {'companylogo': companylogo}
-    local_context.update(d)
+    data = {
+        'companylogo': companylogo,
+        'ultimo_salario':
+            buscar_ultimo_salario(
+                pool, cr, uid, local_context['objects'].mes_do_ano,
+                local_context['objects'].contract_id.id, context
+            ),
+    }
+    local_context.update(data)
