cffbd0e0ea06f422c8cc9c27a18c0ba2746f1cc7
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-04 18:29:01 -0300

    [FIX] Alteração para que o campo da rescisão utilize o campo correto da localização

--- l10n_br_hr_payroll_report/models/res_company.py
+++ l10n_br_hr_payroll_report/models/res_company.py
@@ -6,12 +6,6 @@ from openerp import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    cnae = fields.Integer(
-        string=u'CNAE',
-        size=7,
-        default=3400100,
-    )
-
     cnpj_cei = fields.Char(
         string=u'CNPJ/CEI Tomadora/Obra',
         default='12345678901',
