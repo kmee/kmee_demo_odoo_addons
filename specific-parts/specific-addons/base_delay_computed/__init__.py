8a82498c4bad04ceea66a43fc8575dbf8c361192
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-12-08 07:06:37 -0200

    Add support for delaying new-style computes

--- specific-parts/specific-addons/base_delay_computed/__init__.py
+++ specific-parts/specific-addons/base_delay_computed/__init__.py
@@ -15,7 +15,8 @@ _log = getLogger(__name__)
 
 
 MAP_KEY = 'store_get_delay_map'
-DELAYED_KEY = 'store_set_list'
+DELAYED_KEY = 'delay_store_set_list'
+DELAYED_TODO_KEY = 'delay_recompute_todo'
 
 
 class patch_into(object):
@@ -27,7 +28,7 @@ class patch_into(object):
         self.target = target
 
     def __call__(self, method):
-        _log.debug("patching %s into %s", method.func_name, self.target)
+        _log.info("patching %s into %s", method.func_name, self.target)
         if getattr(self.target, method.func_name, None) is None:
             # regular patching without origin
             setattr(self.target, method.func_name, method)
@@ -52,9 +53,8 @@ def delay_store_compute(self, delay_map):
         MAP_KEY: delayed_model_field_map,
         # Stores the paused old-style fields recomputations to be done later:
         DELAYED_KEY: [],
-        # The key below pauses recomputation of ALL new-style fields.
-        # There is no way to filter only some fields.
-        # 'recompute': False,  # disable new style field recompute
+        # Stores the paused new-style fields recomputations to be done later:
+        DELAYED_TODO_KEY: {},
     }
 
     self = self.with_context(**context_update)
@@ -84,12 +84,12 @@ def delay_store_compute(self, delay_map):
                 done[key] |= todo
                 todo._store_set_values(fields_to_recompute)
 
-    # # recompute paused new-style fields if 'recompute': False in new context
-    # # above:
-    # if old_context.get('recompute', True) and env.recompute:
-    #     # self here doesn't matter, `recompute()` should have been a method
-    #     # on `self.env` instead:
-    #     self.recompute()
+    # recompute delayed new-style fields
+    for field in list(context[DELAYED_TODO_KEY]):
+        recs_list = context[DELAYED_TODO_KEY].pop(field)
+        for recs in recs_list:
+            env.add_todo(field, recs)
+    recompute.origin(self)
 
 
 def _separate_delayed_fields(context, model, fields):
@@ -122,10 +122,46 @@ def _store_get_values(self, cr, uid, ids, fields, context):
         )
         # return the non-delayed now and save the delayed in context for later
         if now_fields:
-            _log.debug("Not delaying:%s", now_fields)
+            _log.debug("_store_get_values(): %r not delaying:%s",
+                       store_model, now_fields)
             result.append((priority, store_model, store_ids, now_fields))
         if later_fields:
             delayed = (priority, store_model, store_ids, later_fields)
-            _log.debug("Delaying:%s", delayed)
+            _log.debug("_store_get_values(): %r delaying:%s",
+                       store_model, delayed)
             context[DELAYED_KEY].append(delayed)
     return result
+
+
+@patch_into(models.BaseModel)
+def recompute(self):
+    context = self.env.context
+    delay_map = context.get(MAP_KEY, {})
+    todo = self.env.all.todo
+
+    delay_fields = {
+        field for field in todo
+        if field.name in delay_map.get(field.model_name, ())
+    }
+
+    for field in list(delay_fields):
+        # Also delay all fields sharing the same compute methods as these.
+        # No need to recurse.
+        delay_fields.update(field.computed_fields)
+
+    field_delay_map = {}
+    for field in delay_fields:
+        recs_list = todo.pop(field, None)
+        if recs_list is not None:
+            field_delay_map[field] = recs_list
+
+    if field_delay_map:
+        _log.debug("%s delaying: %s", recs_list, field.name)
+
+    for field, recs_list in field_delay_map.items():
+        context[DELAYED_TODO_KEY].setdefault(field, []).extend(recs_list)
+
+    if todo:
+        _log.debug('Not delaying recompute of:\n%s', pprint.pformat(todo))
+
+    recompute.origin(self)
