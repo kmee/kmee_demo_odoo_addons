b1eeabf2a26c02f6122effa0e4542f27a3f23baf
Bianca Bartolomei <biancarbartolomei@gmail.com>
2018-07-25 14:28:28 -0300

    [FIX]Path arquivo

--- orede_nfe/wizard/importar_nfe_wizard.py
+++ orede_nfe/wizard/importar_nfe_wizard.py
@@ -10,23 +10,21 @@ import datetime
 from pysped.nfe.leiaute.consrecinfe_310 import ProcNFe
 from openerp.exceptions import Warning as UserError
 
+import os
+import logging
+from openerp.tools.config import config
+_logger = logging.getLogger(__name__)
+
 class ImportarNFe(models.TransientModel):
     _name = 'importar.nfe.wizard'
     _description = 'Importar NFe'
 
-
-    caminho = fields.Char(
-        string='Caminho',
-        help='Caminho do diretório do arquivo de upload. '
-             'Ex: "/home/usuario/documentos/"',
-    )
 
     arquivo_nfe = fields.Many2many(
         'ir.attachment',
         string='Arquivos',
         help='Arquivos com extensão .xml, .zip ou .whl',
     )
-
 
     def importar_nfe(self, cr, uid, ids, context=None):
 
@@ -38,13 +36,14 @@ class ImportarNFe(models.TransientModel):
             if (arq.name.lower().endswith(('.zip', '.whl')) or
                     zipfile.is_zipfile(arq.name)):
 
-                filename = arquivos.caminho + arq.name
+                filename = os.path.join(config.get("data_dir"), 'filestore',
+                                        arquivos._cr.dbname, arq.store_fname)
 
                 try:
                     zipfp = open(filename, 'rb')
                     zip = zipfile.ZipFile(zipfp, allowZip64=True)
 
-                except:
+                except ex:
                     raise UserError(
                             "Arquivo não encontrado. Verifique se o "
                             "caminho está escrito corretamente.")
@@ -69,74 +68,84 @@ class ImportarNFe(models.TransientModel):
             protNFe["message"] = ''
             protNFe["nfe_protocol_number"] = ''
 
-            try:
-                nfe_obj = ProcNFe()
-                nfe_obj.set_xml(inv.decode("utf-8"))
+            nfe_obj = ProcNFe()
+            nfe_obj.set_xml(inv.decode("utf-8"))
 
-                # busca o documento ao qual o xml se refere
-                documento_origem = arquivos.env[
-                    'account.invoice'].search([('internal_number', '=',
-                                                nfe_obj.NFe.infNFe.ide.
-                                                nNF.valor)])
+            # busca o documento ao qual o xml se refere
+            documento_origem = arquivos.env[
+                        'account.invoice'].search([('internal_number', '=',
+                                                    nfe_obj.NFe.infNFe.ide.
+                                                    nNF.valor)])
 
-                # monta o evento do documento de origem
-                vals = {
-                    'type': '1',
-                    'status': nfe_obj.protNFe.infProt.cStat.valor,
-                    'response': '',
-                    'company_id': documento_origem.company_id.id,
-                    'origin': '[NF-E]' + documento_origem.internal_number,
-                    'message': nfe_obj.protNFe.infProt.xMotivo.valor,
-                    'state': 'done',
-                    'document_event_ids': documento_origem.id
-                }
+            if documento_origem:
+                try:
+                    # monta o evento do documento de origem
+                    vals = {
+                            'type': '1',
+                            'status': nfe_obj.protNFe.infProt.cStat.valor,
+                            'response': '',
+                            'company_id': documento_origem.company_id.id,
+                            'origin': '[NF-E]' + documento_origem.internal_number,
+                            'message': nfe_obj.protNFe.infProt.xMotivo.valor,
+                            'state': 'done',
+                            'document_event_ids': documento_origem.id
+                    }
 
-                results.append(vals)
+                    results.append(vals)
 
-                # coloca as informações adicionais do xml
-                protNFe["nfe_access_key"] = nfe_obj.protNFe.infProt.chNFe.valor
-                protNFe["status_code"] = nfe_obj.protNFe.infProt.cStat.valor
-                protNFe["nfe_protocol_number"] = \
-                    nfe_obj.protNFe.infProt.nProt.valor
-                protNFe["message"] = nfe_obj.protNFe.infProt.xMotivo.valor
+                    # coloca as informações adicionais do xml
+                    protNFe["nfe_access_key"] = nfe_obj.protNFe.infProt.chNFe.valor
+                    protNFe["status_code"] = nfe_obj.protNFe.infProt.cStat.valor
+                    protNFe["nfe_protocol_number"] = \
+                        nfe_obj.protNFe.infProt.nProt.valor
+                    protNFe["message"] = nfe_obj.protNFe.infProt.xMotivo.valor
 
-                # muda o estado do documento de origem
-                if nfe_obj.protNFe.infProt.cStat.valor in ('100', '150'):
-                    protNFe["state"] = 'open'
-                    documento_origem.invoice_validate()
-                elif nfe_obj.protNFe.infProt.cStat.valor in ('110', '301','302'):
-                    protNFe["state"] = 'sefaz_denied'
+                    # muda o estado do documento de origem
+                    if nfe_obj.protNFe.infProt.cStat.valor in ('100', '150'):
+                        protNFe["state"] = 'open'
+                        documento_origem.invoice_validate()
+                    elif nfe_obj.protNFe.infProt.cStat.valor in ('110', '301','302'):
+                        protNFe["state"] = 'sefaz_denied'
 
-            except Exception as e:
-                _logger.error(e.message, exc_info=True)
-                vals = {
-                    'type': '-1',
-                    'status': '000',
-                    'response': 'response',
-                    'company_id': documento_origem.company_id.id,
-                    'origin': '[NF-E]' + documento_origem.internal_number,
-                    'file_sent': 'False',
-                    'file_returned': 'False',
-                    'message': 'Erro desconhecido ' + str(e),
-                    'state': 'done',
-                    'document_event_ids': documento_origem.id
-                }
-                results.append(vals)
+                except Exception as e:
+                    _logger.error(e.message, exc_info=True)
+                    vals = {
+                        'type': '-1',
+                        'status': '000',
+                        'response': 'response',
+                        'company_id': documento_origem.company_id.id,
+                        'origin': '[NF-E]' + documento_origem.internal_number,
+                        'file_sent': 'False',
+                        'file_returned': 'False',
+                        'message': 'Erro desconhecido ' + str(e),
+                        'state': 'done',
+                        'document_event_ids': documento_origem.id
+                    }
+                    results.append(vals)
 
-            finally:
-                # escreve os as informações modificadas acima
-                for result in results:
-                    if result['type'] == '0':
-                        event_obj.write(result)
-                    else:
-                        event_obj.create(result)
+                finally:
+                    # escreve os as informações modificadas acima
+                    for result in results:
+                        if result['type'] == '0':
+                            event_obj.write(result)
+                        else:
+                            event_obj.create(result)
 
-                documento_origem.write({
-                    'nfe_status': protNFe["status_code"] + ' - ' +
-                    protNFe["message"],
-                    'nfe_date': datetime.datetime.now(),
-                    'state': protNFe["state"],
-                    'nfe_protocol_number': protNFe["nfe_protocol_number"],
-                    'nfe_access_key': protNFe["nfe_access_key"]
-                })
+                    documento_origem.write({
+                        'nfe_status': protNFe["status_code"] + ' - ' +
+                        protNFe["message"],
+                        'nfe_date': datetime.datetime.now(),
+                        'state': protNFe["state"],
+                        'nfe_protocol_number': protNFe["nfe_protocol_number"],
+                        'nfe_access_key': protNFe["nfe_access_key"]
+                    })
+
+            else:
+                raise UserError(
+                    "Documento de origem ao qual o arquivo de NFe igual "
+                    "a %d "
+                    "nao foi encontrado" % (
+                        nfe_obj.NFe.infNFe.ide.nNF.valor))
+
         return True
+
