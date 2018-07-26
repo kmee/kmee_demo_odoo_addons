7aac8baa52f9b65cb16c21fa5642ab74a9f332ab
Bianca Bartolomei <biancarbartolomei@gmail.com>
2018-07-26 17:17:35 -0300

    [ADD]Descrição do modulo

--- nfe_emissao_manual/__openerp__.py
+++ nfe_emissao_manual/__openerp__.py
@@ -4,7 +4,16 @@
 
 {
     'name': 'NFe Emissão Manual',
-    'description': """""",
+    'description': """
+        Acessar o wizard 
+        Localização Barsil -> Processamento periodico -> Importar NFes
+
+        Quando a tela aparecer pode ser importados arquivos .xml ou .zip quem contenham .xml
+
+        Serão alteradas as inforções da aba documento fiscal na fatura correspondente a nota processada. as informações alteradas são chave de acesso nfe, protocolo, status na sefaz, data do status nfe. 
+
+        Para verificar ir em contabilidade->procurar pela fatura processada->aba do documento fiscal
+        """,
     'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE INFORMATICA',
