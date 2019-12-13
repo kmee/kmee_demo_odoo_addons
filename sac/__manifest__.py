87a85ea228209b576db795c0d39c8df3e65c3480
Luis Felipe Mileo <mileo@kmee.com.br>
2019-12-12 23:02:06 -0300

    [NEW] Base of sac

---
+++ sac/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sac',
+    'summary': """
+        Serviço de Atendimento ao Consumidor""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'www.kmee.com.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
